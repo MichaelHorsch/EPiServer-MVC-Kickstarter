f2c2b1b7f7cdec8c3677964f038382d24a65d93d
MichaelHorsch <mhorsch@gmail.com>
2014-11-18 14:51:04 -0600

    adding url extension

---
+++ Extensions/UrlExtensions.cs
@@ -0,0 +1,17 @@
+﻿using EPiServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPiServerMvcBootstrap.Extensions
+{
+    public static class UrlExtensions
+    {
+        public static string GetFriendlyUrl(this Url url)
+        {
+            return url.ToString().GetFriendlyUrl();
+        }
+    }
+}
